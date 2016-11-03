23d8853317845b71411a9db753c74f51982b39d0
Robert A. Lefebvre <lefebvrera@ornl.gov>
2016-11-02 23:21:48 -0400

    #1 : Revised the FlexBison script to avoid annotation files.

--- src/getpot/FlexBison.sh
+++ src/getpot/FlexBison.sh
@@ -1,3 +1,4 @@
 flex GetPot.lex
 sed -i '' 's/#include <FlexLexer.h>/#include \"wasp\/Flex\/FlexLexer.h\"/' GetPotLexer.cpp 
-bison GetPot.bison --report=state --report=lookahead --report=solved  -g
+bison GetPot.bison
+#bison GetPot.bison --report=state --report=lookahead --report=solved  -g
