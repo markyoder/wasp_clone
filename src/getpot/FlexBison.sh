39cf808fe26fe14476e86800af1a677c1487daba
Robert A. Lefebvre <lefebvrera@ornl.gov>
2016-11-07 10:06:10 -0500

    #1 - Updated lexer generation to be fast. Halves run time for ListGetPot.

--- src/getpot/FlexBison.sh
+++ src/getpot/FlexBison.sh
@@ -1,5 +1,4 @@
-flex --version
-flex GetPot.lex
+flex -Ca -v GetPot.lex
 sed -i '' 's/#include <FlexLexer.h>/#include \"wasp\/Flex\/FlexLexer.h\"/' GetPotLexer.cpp 
 bison --version
 bison GetPot.bison --report=state --report=lookahead
