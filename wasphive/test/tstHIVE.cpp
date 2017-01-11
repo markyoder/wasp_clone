30a4919e4cd148bc926aacd0eee190104b442894
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-01-11 17:57:13 -0500

    #6 begin looking at testing HIVE.

--- wasphive/test/tstHIVE.cpp
+++ wasphive/test/tstHIVE.cpp
@@ -1,12 +1,14 @@
+//#include "waspson/SONInterpreter.h"
 #include "wasphive/HIVE.h"
 #include "waspcore/TreeNodePool.h"
+//#include "waspson/SONNodeView.h"
 #include "gtest/gtest.h"
 #include <iostream>
 #include <string>
 using namespace wasp;
 
 
-TEST(Dummy,dummy)
+TEST(HIVE,instantiation_test)
 {
-
+//    HIVE test;
 }
