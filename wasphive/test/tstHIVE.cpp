3de8af64a45047f22712599d83dc6ffbe8bc5cd7
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-01-12 08:15:16 -0500

    #6 adding configured paths file for hive test suite.

--- wasphive/test/tstHIVE.cpp
+++ wasphive/test/tstHIVE.cpp
@@ -3,10 +3,12 @@
 #include "waspcore/TreeNodePool.h"
 //#include "waspson/SONNodeView.h"
 #include "gtest/gtest.h"
+#include "Paths.h"
 #include <iostream>
 #include <string>
 using namespace wasp;
 
+bool load_resource();
 
 TEST(HIVE,instantiation_test)
 {
