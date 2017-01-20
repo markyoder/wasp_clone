4758a33d1aad07d721fa88ba8df82b5e018623e3
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-01-20 18:24:24 -0500

    correcting package ordering such that GetPot tests can depend on HIVE.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,6 +4,6 @@ TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
      siren waspsiren   PT OPTIONAL
      expr  waspexpr    PT OPTIONAL
      son   waspson     PT OPTIONAL
-     getpot waspgetpot PT OPTIONAL
      hive  wasphive    PT OPTIONAL
+     getpot waspgetpot PT OPTIONAL     
 )
