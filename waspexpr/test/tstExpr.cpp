9d7ef244912c10eaae2202240fb13dce33acc889
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-02-20 21:57:23 -0500

    WIP #20 adding test of multiplication.

--- waspexpr/test/tstExpr.cpp
+++ waspexpr/test/tstExpr.cpp
@@ -106,6 +106,13 @@ TEST(ExprInterpreter,mult)
     ASSERT_EQ(wasp::VALUE, op.child_at(0).type());
     ASSERT_EQ(wasp::MULTIPLY, op.child_at(1).type());
     ASSERT_EQ(wasp::VALUE, op.child_at(2).type());
+    auto result = interpreter.evaluate();
+    ASSERT_TRUE(result.is_integer());
+    ASSERT_TRUE(result.is_number());
+    ASSERT_FALSE(result.is_real());
+    ASSERT_FALSE(result.is_string());
+    ASSERT_FALSE(result.is_error());
+    ASSERT_EQ(24, result.integer());
 }
 TEST(ExprInterpreter,div)
 {
