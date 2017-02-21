09cf8145728d81b97ee268daef45f46973f0cb51
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-02-20 21:38:45 -0500

    WIP #20 adding test of exponentiation.

--- waspexpr/test/tstExpr.cpp
+++ waspexpr/test/tstExpr.cpp
@@ -83,6 +83,13 @@ TEST(ExprInterpreter,exponent)
     ASSERT_EQ(wasp::VALUE, op.child_at(0).type());
     ASSERT_EQ(wasp::EXPONENT, op.child_at(1).type());
     ASSERT_EQ(wasp::VALUE, op.child_at(2).type());
+    auto result = interpreter.evaluate();
+    ASSERT_TRUE(result.is_integer());
+    ASSERT_TRUE(result.is_number());
+    ASSERT_FALSE(result.is_real());
+    ASSERT_FALSE(result.is_string());
+    ASSERT_FALSE(result.is_error());
+    ASSERT_EQ(9, result.integer());
 }
 TEST(ExprInterpreter,mult)
 {
