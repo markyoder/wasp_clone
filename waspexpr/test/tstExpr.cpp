3410334c56ae9dba21f4b0e66c4533f5544b8115
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-02-20 22:05:03 -0500

    WIP #20 adding test of division.

--- waspexpr/test/tstExpr.cpp
+++ waspexpr/test/tstExpr.cpp
@@ -129,6 +129,13 @@ TEST(ExprInterpreter,div)
     ASSERT_EQ(wasp::VALUE, op.child_at(0).type());
     ASSERT_EQ(wasp::DIVIDE, op.child_at(1).type());
     ASSERT_EQ(wasp::VALUE, op.child_at(2).type());
+    auto result = interpreter.evaluate();
+    ASSERT_TRUE(result.is_integer());
+    ASSERT_TRUE(result.is_number());
+    ASSERT_FALSE(result.is_real());
+    ASSERT_FALSE(result.is_string());
+    ASSERT_FALSE(result.is_error());
+    ASSERT_EQ(2, result.integer());
 }
 TEST(ExprInterpreter,equal)
 {
