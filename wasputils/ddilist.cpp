fed8ceba3b3a0b3c79ea40bbfa964bd7ce1ccdf5
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-03-02 22:09:53 -0500

    #35 initial addition of ddilist utility.

---
+++ wasputils/ddilist.cpp
@@ -0,0 +1,49 @@
+/* 
+ * File:   ddilist.cpp
+ * Author: raq
+ *
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include "waspddi/DDInterpreter.h"
+#include "waspson/SONInterpreter.h"
+using namespace std;
+using namespace wasp;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    if( argc != 3 ){
+        std::cout<<"Usage: "<<std::endl;
+        std::cout<<"\tDDIList schema inputFile "<<std::endl;
+        std::cout<<"\ti.e., ddilist /path/to/definition.son /path/to/some/input "<<std::endl;
+        return 1;
+    }
+    
+    DDInterpreter<TreeNodePool<unsigned int, unsigned int
+            ,TokenPool<unsigned int,unsigned int, unsigned int>>> parser;
+
+    SONInterpreter<TreeNodePool<unsigned int, unsigned int
+            ,TokenPool<unsigned int,unsigned int, unsigned int>>> schema;
+    bool schema_failed = !schema.parseFile(argv[1]);
+    if( schema_failed )
+    {
+        std::cout<<"***Error : Parsing of "<<argv[1]<<" failed!"<<std::endl;
+        return 1;
+    }
+    bool failed = !parser.parseFile(argv[2]);
+    if( failed ){
+        std::cout<<"***Error : Parsing of "<<argv[2]<<" failed!"<<std::endl;
+        return 1;
+    }
+    parser.definition()->create_from(schema.root());
+    parser.root().paths(std::cout);
+    
+    return 0;
+}
+
+
