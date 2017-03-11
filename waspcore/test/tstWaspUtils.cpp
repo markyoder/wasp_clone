b7e00549ead1e9184bb47174578378174ec75a0f
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-03-11 14:20:14 -0500

    #19 initial format capability for fixed precision statements.

--- waspcore/test/tstWaspUtils.cpp
+++ waspcore/test/tstWaspUtils.cpp
@@ -19,3 +19,48 @@ TEST( utils, string_wilds)
     EXPECT_TRUE(wildcard_string_match("*", "could_be_anything")); // Yes
 }
 
+
+template<class T>
+struct format_test{
+    std::string format;
+    T arg;
+    std::string expected;
+    format_test(const std::string & f, T a, const std::string & e)
+    {
+        format = f;
+        arg = a;
+        expected = e;
+    }
+};
+
+TEST( utils, format )
+{
+    std::vector<format_test<double>> tests={
+        {"|%7f|",3.14159265,"|3.141593|"}
+       ,{"|%8f|",3.14159265,"|3.141593|"}
+       ,{"|%9f|",3.14159265,"| 3.141593|"}
+       ,{"|%10f|",3.14159265,"|  3.141593|"}
+       ,{"|%.0f|",3.14159265,"|3|"}
+       ,{"|%.1f|",3.14159265,"|3.1|"}
+       ,{"|%.2f|",3.14159265,"|3.14|"}
+       ,{"|%.3f|",3.14159265,"|3.142|"}
+       ,{"|%1.0f|",3.14159265,"|3|"}
+       ,{"|%4.1f|",3.14159265,"| 3.1|"}
+       ,{"|%4.8f|",3.14159265,"|3.14159265|"}
+       ,{"|%8.2f|",3.14159265,"|    3.14|"}
+       ,{"|%10.3f|",3.14159265,"|     3.142|"}
+       ,{"|%01.0f|",3.14159265,"|3|"}
+       ,{"|%04.1f|",3.14159265,"|03.1|"}
+       ,{"|%04.8f|",3.14159265,"|3.14159265|"}
+       ,{"|%08.2f|",3.14159265,"|00003.14|"}
+       ,{"|%010.3f|",3.14159265,"|000003.142|"}
+    };
+    for( const auto & tst : tests )
+    {
+        SCOPED_TRACE(tst.format);
+        std::cout<<"Testing Format of "<<tst.format<<std::endl;
+        std::stringstream out, err;
+        ASSERT_TRUE(wasp::format(out,err,tst.format.c_str(),tst.arg));
+        EXPECT_EQ( tst.expected, out.str() );
+    }
+}
--- waspcore/utils.h
+++ waspcore/utils.h
@@ -1,7 +1,11 @@
 #ifndef WASP_UTILS_H
 #define WASP_UTILS_H
+#include "waspcore/wasp_bug.h"
 #include <string>
 #include <sstream>
+#include <iostream>
+#include <iomanip>
+
 namespace wasp{
     /**
      * @brief strip_quotes removes single or double quotes
@@ -103,5 +107,164 @@ namespace wasp{
                     || wildcard_string_match(first, second+1);
         return false;
     }
+
+    /**
+     * @brief format variadic template function for type safe printf/Java.Format like capability
+     * @param out - the output stream to emit the formatted result
+     * @param err - the error stream if an error occurs
+     * @param s - the format string
+     * @param value the argument to print
+     * @param args the additional arguments to print
+     * NOTE : format(std::cout, "%%4.8f=|%4.8f|",3.14159265)  => "%4.1f=| 3.1|"
+     */
+    template<typename T>
+    bool format(std::ostream& out,std::ostream& err, const char *s, T value)
+    {
+        int width = out.width();
+        int prec  = out.precision();
+        wasp_line("format :: width="<<width<<", prec="<<prec);
+        static char conversion_types[] = "dfsg";
+        while (*s) {
+            // are we encountering a possible format statement
+            // %[flags][width][.precision]conversion is lexed and parsed where
+            //   flags : justification, sign, padding and parenthesis
+            //   '-' The result will be left-justified
+            //   '+' The result will always include a sign
+            //   ' ' The result will include a leading space for positive values
+            //   '0' The result will be zero-padded
+            //   '(' The result will enclose negative numbers in parentheses
+            //   width : [1-9][0-9]* (cannot start with 0)
+            bool include_sign = false; // true when '%+...' is specified
+            bool include_lead_space_for_positive = false; // true when '% ...' is specified
+            bool include_parenthesis_for_negative = false; // true when '%(...' is specified
+            wasp_line("s="<<*s);
+            if (*s == '%') {
+                // Check for escaped formatting '%%'
+                wasp_line("###found format expression");
+                const char * sl = s + 1; // string lookahead char
+                if ( *sl== '%') {
+                    s=sl+1; // move beyond
+                    continue;
+                }
+                while( *sl )
+                {
+                    wasp_line("sl="<<*sl);
+                    // check for flag
+                    if( *sl == '-' )
+                    {   // %-..., left justified
+                        out<<std::left;
+                        ++sl;
+                    }
+                    else if( *sl == '+' )
+                    {   // %+..., include plus for positive numbers
+                        include_sign = true;
+                        ++sl;
+                    }
+                    else if( *sl == ' ' )
+                    {   // include leading space for positive numbers
+                        include_lead_space_for_positive = true;
+                        ++sl;
+                    }
+                    else if( *sl == '0' )
+                    {   // pad space with zeros
+                        out.fill('0');
+                        ++sl;
+                    }
+                    else if( *sl == '(')
+                    {   // surround negative numbers with parenthesis
+                        include_parenthesis_for_negative = true;
+                        ++sl;
+                    }
+                    else if( *sl == '.') // check for %.'precision''conversion'
+                    {   // no width, just precision
+                        ++sl; // move beyond '.'
+                        const char * p = sl; // cache location of the start of the precision
+                        // isdigit returns non-zero when character is a digit
+                        while( sl && std::isdigit(*sl) )
+                        {
+                            ++sl; // move forward
+                        }
+                        // check for '%.conversion'| '%.NULL' - missing precision
+                        if( p == sl )
+                        {  // if no difference in character pointers, error
+                            err<<"missing precision format statement at index "<<(sl-p);
+                            return false;
+                        }
+                        // the precision should be terminated by the type 'conversion'
+                        // check that the 'conversion' is supported
+                        if( std::strchr(conversion_types,*sl) == nullptr )
+                        {
+                            err<<"unsupported format conversion type ("<<*sl<<"), must be one of "<<conversion_types;
+                            return false;
+                        }
+                        // capture the precision
+                        std::stringstream prec_substr(std::string(p,sl));
+                        prec_substr>>prec;
+                        wasp_check( !prec_substr.bad() || !prec_substr.fail() );
+                    }
+                    else if ( std::isdigit(*sl) ) // check for width
+                    {
+                        const char * w = sl;
+                        ++sl;
+                        while( std::isdigit(*sl) )
+                        {
+                            ++sl;
+                        }
+                        // capture the precision
+                        std::string substr(w,sl);
+                        wasp_line("format width str '"<<substr<<"'");
+                        std::stringstream width_substr(substr);
+                        width_substr>>width;
+                        wasp_line("WIDTH="<<width);
+                        wasp_check( !width_substr.bad() || !width_substr.fail() );
+                    }
+                    else if( std::strchr(conversion_types,*sl) != nullptr )
+                    {
+                        // a format expression has been consumed, emit the value
+                        wasp_line("width = "<<width<<", prec="<<prec);
+                        out<<std::setw(width)<<std::setprecision(prec);
+                        switch( *sl )
+                        {
+                        case 'g':
+                            out<<std::scientific;
+                            break;
+                        case 'f':
+                            out<<std::fixed;
+                            break;
+                        case 's':
+
+                            break;
+                        }
+
+                        if( include_parenthesis_for_negative
+                                && std::is_fundamental<T>::value
+                                && value < 0)
+                        {
+                            out<<"("<<value<<")";
+                        }
+                        else {
+                            out<<value;
+                        }
+                        break; // break from lookahead loop
+                    }else{
+                        err<<"'"<<*sl<<"' is an unknown format element";
+                        return false;
+                    }
+                }// end of format lookahead loop
+                s = sl; // move forward the lookahead amount
+                // ensure we concluded on a conversion type
+                if( std::strchr(conversion_types,*sl) == nullptr ){
+                    err<<"format type conversion is missing";
+                    wasp_line("error at lookahead loop conclusion.");
+                    return false;
+                }
+                ++s;
+            }else{
+                // emit intermediate characters
+                out << *s++;
+            }
+        } // eo while loop
+        return true;
+    } // end of fmt
 }
 #endif
