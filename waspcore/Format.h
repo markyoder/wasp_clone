75d4d45bd7e9325a43dbce6d6dc95075185ea228
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-03-11 21:03:00 -0500

    #19 split format logic into static class for privatization of internal methods and preparation for string type specialization.

---
+++ waspcore/Format.h
@@ -0,0 +1,260 @@
+#ifndef WASPCORE_FORMAT_H
+#define WASPCORE_FORMAT_H
+#include "waspcore/wasp_bug.h"
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <stdio.h> // windows exponent configure
+
+namespace wasp{
+    /**
+     * @brief The Format class is a static class to privatize implementation details
+     */
+    class Format{
+
+    public:
+
+    /**
+     * @brief format variadic template function for type safe printf/Java.Format like capability
+     * @param out - the output stream to emit the formatted result
+     * @param err - the error stream if an error occurs
+     * @param s - the format string
+     * @param value the argument to print
+     * @param args the additional arguments to print
+     * NOTE : format(std::cout, "%%4.8f=|%4.8f|",3.14159265)  => "%4.1f=| 3.1|"
+     */
+    template<typename T>
+    static bool fmt(std::ostream& out,std::ostream& err, const char *s, const T& value)
+    {
+        //if Windows, only output two-digits of exponent if possible like linux
+    #ifdef _WIN32
+        unsigned int old_exponent_format;
+        old_exponent_format = _set_output_format(_TWO_DIGIT_EXPONENT);
+    #endif
+        int width = out.width();
+        int prec  = out.precision();
+        static char conversion_types[] = "dfsge";
+        bool error_occurred = false;
+        while ( *s && error_occurred == false ) {
+            // are we encountering a possible format statement
+            // %[flags][width][.precision]conversion is lexed and parsed where
+            //   flags : justification, sign, padding and parenthesis
+            //   '-' The result will be left-justified
+            //   '+' The result will always include a sign
+            //   ' ' The result will include a leading space for positive values
+            //   '0' The result will be zero-padded
+            //   '(' The result will enclose negative numbers in parentheses
+            //   width : [1-9][0-9]* (cannot start with 0)
+            bool include_sign = false; // true when '%+...' is specified
+            bool include_lead_space_for_positive = false; // true when '% ...' is specified
+            bool include_parenthesis_for_negative = false; // true when '%(...' is specified
+            wasp_line("s="<<*s);
+            if (*s == '%') {
+                // Check for escaped formatting '%%'
+                const char * sl = s + 1; // string lookahead char
+                if ( *sl== '%') {
+                    out << *sl; // emit escaped '%'
+                    s=sl+1; // move beyond
+                    continue;
+                }
+                while( *sl )
+                {
+                    wasp_line("sl="<<*sl);
+                    // check for flag
+                    if( *sl == '-' )
+                    {   // %-..., left justified
+                        out<<std::left;
+                        ++sl;
+                    }
+                    else if( *sl == '+' )
+                    {   // %+..., include plus for positive numbers
+                        include_sign = true;
+                        ++sl;
+                    }
+                    else if( *sl == ' ' )
+                    {   // include leading space for positive numbers
+                        include_lead_space_for_positive = true;
+                        ++sl;
+                    }
+                    else if( *sl == '0' )
+                    {   // pad space with zeros
+                        out.fill('0');
+                        out<<std::internal;
+                        ++sl;
+                    }
+                    else if( *sl == '(')
+                    {   // surround negative numbers with parenthesis
+                        include_parenthesis_for_negative = true;
+                        ++sl;
+                    }
+                    else if( *sl == '.') // check for %.'precision''conversion'
+                    {   // no width, just precision
+                        ++sl; // move beyond '.'
+                        const char * p = sl; // cache location of the start of the precision
+                        // isdigit returns non-zero when character is a digit
+                        while( sl && std::isdigit(*sl) )
+                        {
+                            ++sl; // move forward
+                        }
+                        // check for '%.conversion'| '%.NULL' - missing precision
+                        if( p == sl )
+                        {  // if no difference in character pointers, error
+                            err<<"missing precision format statement at index "<<(sl-p);
+                            error_occurred = true;
+                            break;
+                        }
+                        // the precision should be terminated by the type 'conversion'
+                        // check that the 'conversion' is supported
+                        if( std::strchr(conversion_types,*sl) == nullptr )
+                        {
+                            err<<"unsupported format conversion type ("<<*sl<<"), must be one of "<<conversion_types;
+                            error_occurred = true;
+                            break;
+                        }
+                        // capture the precision
+                        std::stringstream prec_substr(std::string(p,sl));
+                        prec_substr>>prec;
+                        wasp_check( !prec_substr.bad() || !prec_substr.fail() );
+
+                    }
+                    else if ( std::isdigit(*sl) ) // check for width
+                    {
+                        const char * w = sl; // cache beginning of width substr
+                        ++sl;
+                        while( std::isdigit(*sl) )
+                        {
+                            ++sl;
+                        }
+                        // capture the precision
+                        std::string substr(w,sl);
+                        std::stringstream width_substr(substr);
+                        width_substr>>width;
+                        wasp_check( !width_substr.bad() || !width_substr.fail() );
+                    }
+                    else if( std::strchr(conversion_types,*sl) != nullptr )
+                    {
+                        // a format expression has been consumed, emit the value
+                        error_occurred = !fmt_emit(out,err
+                                               ,*sl
+                                               ,value
+                                               ,width
+                                               ,prec
+                                               ,include_parenthesis_for_negative
+                                               ,include_lead_space_for_positive
+                                               ,include_sign);
+                        break; // break from lookahead loop
+                    }else{
+                        err<<"'"<<*sl<<"' is an unknown format element";
+                        error_occurred = true;
+                        break;
+                    }
+                }// end of format lookahead loop
+                s = sl; // move forward the lookahead amount
+                // ensure we concluded on a conversion type
+                if( std::strchr(conversion_types,*sl) == nullptr ){
+                    err<<"format type conversion is missing";
+                    error_occurred = true;
+                    break;
+                }
+                ++s;
+            }else{
+                // emit intermediate characters
+                out << *s++;
+            }
+        } // eo while loop
+#ifdef _WIN32
+    _set_output_format( old_exponent_format );
+#endif
+        return error_occurred == false;
+    } // end of format
+
+    private:
+    template<typename T>
+    static bool fmt_emit(std::ostream& out,std::ostream& err, char format, const T& value
+                  , int width
+                  , int prec
+                  , bool include_parenthesis_for_negative
+                  , bool include_lead_space_for_positive
+                  , bool include_sign
+                  )
+    {
+        switch( format )
+        {
+        case 'e':
+            out<<std::scientific;
+            break;
+        case 'f':
+            out<<std::fixed;
+            break;
+        case 'g':
+        {
+            if( std::is_fundamental<T>::value  == false )
+            {
+                err<<"format argument is not a fundamental type";
+                return false;
+            }
+            int magnitude = std::floor(std::log10(value));
+            if(  magnitude < -4
+                    && prec >= std::abs(magnitude))
+            {
+                out<<std::scientific;
+                --prec;
+            }
+            else if( magnitude <= -4 )
+            {
+                out<<std::fixed;
+                prec +=3;
+            }
+            else
+            {
+                out<<std::fixed;
+                if( prec > 0 ) prec -=magnitude+1;
+            }
+            break;
+        }
+        case 's':
+            // nothing special to see here ... yet
+            break;
+        case 'd':
+            out<<std::fixed;
+            prec = 0; // float -> integer
+            break;
+        }
+
+        if( include_parenthesis_for_negative
+                && value < 0)
+        {
+            width -=3; // account for ()
+            if( width < 0 ) width = 0;
+            out<<std::setw(width)<<std::setprecision(prec);
+            out<<"("<<-value<<")";
+        }
+        else if( include_sign
+                 && value > 0 )
+        {
+            width-=2; // account for '+'
+            if( width < 0 ) width = 0;
+            out<<std::setw(width)<<std::setprecision(prec);
+            out<<'+'<<value;
+        }
+        else if (include_lead_space_for_positive
+                 && value > 0 )
+        {
+            // only adjust for leading ws if no width already
+            width -=2;
+            if( width < 0 ) width = 0;
+            out<<std::setw(width)<<std::setprecision(prec);
+            out<<" "<<value;
+        }
+        else {
+            out<<std::setw(width)<<std::setprecision(prec);
+            out<<value;
+        }
+        return true;
+    }
+}; // end of class Foramt
+} // end of namespace
+#endif
---
+++ waspcore/test/tstFormat.cpp
@@ -0,0 +1,200 @@
+#include "gtest/gtest.h"
+
+#include "waspcore/Format.h"
+using namespace wasp;
+
+template<class T>
+struct format_test{
+    std::string format;
+    T arg;
+    std::string expected;
+    format_test(const std::string & f, T a, const std::string & e)
+    {
+        format = f;
+        arg = a;
+        expected = e;
+    }
+};
+
+TEST( utils, format_fixed_precision )
+{
+    std::vector<format_test<double>> tests={
+        {"|%f|",3.14159265,"|3.141593|"}
+       ,{"|%7f|",3.14159265,"|3.141593|"}
+       ,{"|%8f|",3.14159265,"|3.141593|"}
+       ,{"|%9f|",3.14159265,"| 3.141593|"}
+       ,{"|%10f|",3.14159265,"|  3.141593|"}
+       ,{"|%.0f|",3.14159265,"|3|"}
+       ,{"|%.1f|",3.14159265,"|3.1|"}
+       ,{"|%.2f|",3.14159265,"|3.14|"}
+       ,{"|%.3f|",3.14159265,"|3.142|"}
+       ,{"|%1.0f|",3.14159265,"|3|"}
+       ,{"|%4.1f|",3.14159265,"| 3.1|"}
+       ,{"|%4.8f|",3.14159265,"|3.14159265|"}
+       ,{"|%8.2f|",3.14159265,"|    3.14|"}
+       ,{"|%10.3f|",3.14159265,"|     3.142|"}
+       ,{"|%01.0f|",3.14159265,"|3|"}
+       ,{"|%04.1f|",3.14159265,"|03.1|"}
+       ,{"|%04.8f|",3.14159265,"|3.14159265|"}
+       ,{"|%08.2f|",3.14159265,"|00003.14|"}
+       ,{"|%010.3f|",3.14159265,"|000003.142|"}
+    };
+    for( const auto & tst : tests )
+    {
+        SCOPED_TRACE(tst.format);
+        std::cout<<"Testing fixed format of "<<tst.format<<std::endl;
+        std::stringstream out, err;
+        ASSERT_TRUE(wasp::Format::fmt(out,err,tst.format.c_str(),tst.arg));
+        EXPECT_EQ( tst.expected, out.str() );
+    }
+}
+
+TEST( utils, format_general_scientific_precision )
+{
+    std::vector<format_test<double>> tests={
+        {"|%g|",3.14159265,"|3.14159|"}
+        ,{"|%.0g|",3.14159265,"|3|"}
+        ,{"|%.1g|",3.14159265,"|3|"}
+        ,{"|%.2g|",3.14159265,"|3.1|"}
+        ,{"|%.3g|",3.14159265,"|3.14|"}
+        ,{"|%1.0g|",3.14159265,"|3|"}
+        ,{"|%4.1g|",3.14159265,"|   3|"}
+        ,{"|%4.8g|",3.14159265,"|3.1415927|"}
+        ,{"|%8.2g|",3.14159265,"|     3.1|"}
+        ,{"|%10.3g|",3.14159265,"|      3.14|"}
+        ,{"|%01.0g|",3.14159265,"|3|"}
+        ,{"|%04.1g|",3.14159265,"|0003|"}
+        ,{"|%04.8g|",3.14159265,"|3.1415927|"}
+        ,{"|%08.2g|",3.14159265,"|000003.1|"}
+        ,{"|%010.3g|",3.14159265,"|0000003.14|"}
+        ,{"|%.4g|",1e-4,"|0.0001000|"}
+        ,{"|%10.4g|",1e-4,"| 0.0001000|"}
+        ,{"|%.5g|",1e-4,"|0.00010000|"}
+        ,{"|%.5g|",1e-5,"|1.0000e-05|"}
+        ,{"|%.6g|",1e-6,"|1.00000e-06|"}
+        ,{"|%.7g|",1e-2,"|0.01000000|"}
+        ,{"|%.7g|",1e-2,"|0.01000000|"}
+        ,{"|%10.7g|",1e-2,"|0.01000000|"}
+        ,{"|%10.7g|",1e2,"|  100.0000|"}
+
+    };
+    for( const auto & tst : tests )
+    {
+        SCOPED_TRACE(tst.format);
+        std::cout<<"Testing general scientific format of "<<tst.format<<std::endl;
+        std::stringstream out, err;
+        ASSERT_TRUE(wasp::Format::fmt(out,err,tst.format.c_str(),tst.arg));
+        EXPECT_EQ( tst.expected, out.str() );
+    }
+}
+
+TEST( utils, format_scientific_precision )
+{
+    std::vector<format_test<double>> tests={
+        {"|%e|",3.14159265,"|3.141593e+00|"}
+        ,{"|%.0e|",3.14159265,"|3e+00|"}
+        ,{"|%.1e|",3.14159265,"|3.1e+00|"}
+        ,{"|%.2e|",3.14159265,"|3.14e+00|"}
+        ,{"|%.3e|",3.14159265,"|3.142e+00|"}
+        ,{"|%1.0e|",3.14159265,"|3e+00|"}
+        ,{"|%4.1e|",3.14159265,"|3.1e+00|"}
+        ,{"|%4.8e|",3.14159265,"|3.14159265e+00|"}
+        ,{"|%8.2e|",3.14159265,"|3.14e+00|"}
+        ,{"|%10.3e|",3.14159265,"| 3.142e+00|"}
+        ,{"|%01.0e|",3.14159265,"|3e+00|"}
+        ,{"|%04.1e|",3.14159265,"|3.1e+00|"}
+        ,{"|%04.8e|",3.14159265,"|3.14159265e+00|"}
+        ,{"|%08.2e|",3.14159265,"|3.14e+00|"}
+        ,{"|%010.3e|",3.14159265,"|03.142e+00|"}
+        ,{"|%.4e|",1e-4,"|1.0000e-04|"}
+        ,{"|%10.4e|",1e-4,"|1.0000e-04|"}
+        ,{"|%.5e|",1e-4,"|1.00000e-04|"}
+        ,{"|%.5e|",1e-5,"|1.00000e-05|"}
+        ,{"|%.6e|",1e-6,"|1.000000e-06|"}
+        ,{"|%.7e|",1e-2,"|1.0000000e-02|"}
+        ,{"|%.7e|",1e-2,"|1.0000000e-02|"}
+        ,{"|%10.7e|",1e-2,"|1.0000000e-02|"}
+        ,{"|%10.7e|",1e2,"|1.0000000e+02|"}
+    };
+
+    for( const auto & tst : tests )
+    {
+        SCOPED_TRACE(tst.format);
+        std::cout<<"Testing scientific format of "<<tst.format<<std::endl;
+        std::stringstream out, err;
+        ASSERT_TRUE(wasp::Format::fmt(out,err,tst.format.c_str(),tst.arg));
+        EXPECT_EQ( tst.expected, out.str() );
+    }
+}
+
+TEST( utils, format_double_as_integer )
+{
+    std::vector<format_test<double>> tests={
+        {"|%d|",3.14159265,"|3|"}
+        ,{"|%d|",1e-4,"|0|"}
+    };
+
+    for( const auto & tst : tests )
+    {
+        SCOPED_TRACE(tst.format);
+        std::cout<<"Testing double as integer format of "<<tst.format<<std::endl;
+        std::stringstream out, err;
+        ASSERT_TRUE(wasp::Format::fmt(out,err,tst.format.c_str(),tst.arg));
+        EXPECT_EQ( tst.expected, out.str() );
+    }
+}
+
+TEST( utils, format_integer )
+{
+    std::vector<format_test<int>> tests={
+        {"|%d|",3,"|3|"}
+        ,{"|%1d|",30,"|30|"}
+        ,{"|%5d|",30,"|   30|"}
+        ,{"|%-5d|",30,"|30   |"}
+        ,{"|%(5d|",-30,"| (30)|"}
+        ,{"|%+5d|", 30,"|  +30|"}
+        ,{"|%+5d|",-30,"|  -30|"}
+        ,{"|%05d|",-30,"|-0030|"}
+        ,{"%%5d=|%05d|",-30,"%5d=|-0030|"}
+        ,{"|% d|",30,"| 30|"}
+        ,{"|% 3d|",30,"| 30|"}
+        ,{"|% 4d|",30,"|  30|"}
+        ,{"|% 10d|",30,"|        30|"}
+    };
+
+    for( const auto & tst : tests )
+    {
+        SCOPED_TRACE(tst.format);
+        std::cout<<"Testing integer format of "<<tst.format<<std::endl;
+        std::stringstream out, err;
+        ASSERT_TRUE(wasp::Format::fmt(out,err,tst.format.c_str(),tst.arg));
+        EXPECT_EQ( tst.expected, out.str() );
+    }
+}
+TEST( utils, DISABLED_format_string )
+{
+    std::vector<format_test<std::string>> tests={
+        {"|%s|","3","|3|"}
+        ,{"|%1s|","30","|30|"}
+        ,{"|%5s|","30","|   30|"}
+        ,{"|%-5s|","30","|30   |"}
+        ,{"|%(5s|","-30","| (30)|"}
+        ,{"|%+5s|","30","|  +30|"}
+        ,{"|%+5s|","-30","|  -30|"}
+        ,{"|%05s|","-30","|00-30|"}
+        ,{"%%5s=|%05s|","-30","%5d=|00-30|"}
+        ,{"|% s|","30","| 30|"}
+        ,{"|% 3s|","30","| 30|"}
+        ,{"|% 4s|","30","|  30|"}
+        ,{"|% 10s|","30","|        30|"}
+    };
+
+    for( const auto & tst : tests )
+    {
+        SCOPED_TRACE(tst.format);
+        std::cout<<"Testing string format of "<<tst.format<<std::endl;
+        std::stringstream out, err;
+//        ASSERT_TRUE(wasp::Format::fmt(out,err,tst.format.c_str(),tst.arg));
+        EXPECT_EQ( tst.expected, out.str() );
+    }
+}
