d2d265fd45c8796be623140f588ba5b1d8aaf63a
Robert A. Lefebvre <lefebvrera@ornl.gov>
2017-05-10 20:43:12 -0400

    #53 adding initial jsonxml utility.

---
+++ wasputils/jsonxml.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   jsonlist.cpp
+ * Author: Robert A. Lefebvre
+ *
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include "waspjson/JSONInterpreter.h"
+using namespace std;
+using namespace wasp;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    if( argc != 2 ){
+        std::cout<<"Usage: "<<std::endl;
+        std::cout<<"\tJSONList inputFile "<<std::endl;
+        std::cout<<"\ti.e., jsonlist /path/to/some/input "<<std::endl;
+        return 1;
+    }
+    JSONInterpreter<TreeNodePool<unsigned int, unsigned int
+            ,TokenPool<unsigned int,unsigned int, unsigned int>>> parser;
+    bool failed = !parser.parseFile(argv[1]);
+    if( failed ){
+        std::cout<<"***Error : Parsing of "<<argv[1]<<" failed!"<<std::endl;
+        return 1;
+    }
+    wasp::to_xml(parser.root(),std::cout);
+    return 0;
+}
+
+
