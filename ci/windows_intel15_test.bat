054098f5920b01fcfcd2a98a6edbea67dda3dc09
Robert A Lefebvre <raq@ornl.gov>
2017-10-18 19:04:13 -0400

    #39 adding back intel 15 test execution logic.

--- ci/windows_intel15_test.bat
+++ ci/windows_intel15_test.bat
@@ -4,7 +4,21 @@ set PATH=c:\vendors\cmake-ninja\bin;C:\Program Files (x86)\NSIS;%PATH%
 
 call "C:\Program Files (x86)\Intel\Composer XE 2015\bin\ipsxe-comp-vars.bat" intel64 vs2013
 
-
+cmake -DCMAKE_C_COMPILER=icl ^
+      -DCMAKE_CXX_COMPILER=icl ^
+	  -DBUILDNAME="Windows-Intel-15.0-Release-%CI_BUILD_REF_NAME%" ^
+	  -DCMAKE_BUILD_TYPE:STRING=RELEASE ^
+	  -Dwasp_ENABLE_TESTS:BOOL=ON ^
+	  -Dwasp_ENABLE_ALL_PACKAGES:BOOL=ON ^
+	  -G "Ninja" ^
+	  ..
+ctest -D ExperimentalStart ^
+      -D ExperimentalBuild ^
+	  -D ExperimentalTest ^
+	  -D ExperimentalSubmit
+cd ..\
+rd /s /q build
+mkdir build && cd build
 cmake -DCPACK_GENERATOR=STGZ ^
 	  -DCPACK_PACKAGE_NAME=WASP ^
 	  -DCMAKE_C_COMPILER=icl -D CMAKE_CXX_COMPILER=icl ^
