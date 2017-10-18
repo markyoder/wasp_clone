d309916a75336e399c0d59a8572ea2623f957478
Lefebvre, Robert A <lefebvrera@ornl.gov>
2017-10-18 12:56:30 -0400

    #39 consolidated vs2013 test logic.

---
+++ ci/setup.bat
@@ -0,0 +1,10 @@
+ where git
+ git --version
+ where python
+ python --version
+ where cmake
+ cmake --version
+ git clone https://github.com/lefebvre/TriBITS.git TriBITS
+ git clone https://github.com/lefebvre/testframework testframework
+ git clone https://github.com/lefebvre/googletest googletest
+ mkdir build
