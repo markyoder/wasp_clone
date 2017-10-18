0c09cee85e6c6a346f41fc8b4cd4db7364658f21
Lefebvre, Robert A <lefebvrera@ornl.gov>
2017-10-18 12:58:32 -0400

    #39 removing extraneous whitespace in *.bat files

--- ci/vs2013_test.bat
+++ ci/vs2013_test.bat
@@ -1,5 +1,5 @@
- .\ci\setup.bat
- cd build
- call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
- cmake -DBUILDNAME="Windows-CL-18-Release-%CI_BUILD_REF_NAME%" -DCMAKE_BUILD_TYPE:STRING=RELEASE -Dwasp_ENABLE_TESTS:BOOL=ON -Dwasp_ENABLE_ALL_PACKAGES:BOOL=ON -G "Visual Studio 12 2013 Win64" ..
- ctest -D ExperimentalStart -D ExperimentalBuild -D ExperimentalTest -D ExperimentalSubmit:
+.\ci\setup.bat
+cd build
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+cmake -DBUILDNAME="Windows-CL-18-Release-%CI_BUILD_REF_NAME%" -DCMAKE_BUILD_TYPE:STRING=RELEASE -Dwasp_ENABLE_TESTS:BOOL=ON -Dwasp_ENABLE_ALL_PACKAGES:BOOL=ON -G "Visual Studio 12 2013 Win64" ..
+ctest -D ExperimentalStart -D ExperimentalBuild -D ExperimentalTest -D ExperimentalSubmit:
