17360a70e97eb86e27bbb0cd862a19dbad67cb9c
Lefebvre, Robert A <lefebvrera@ornl.gov>
2017-10-18 19:22:12 -0400

    #39 reformatting mac_clang_test script.

--- ci/mac_clang_test.sh
+++ ci/mac_clang_test.sh
@@ -1,4 +1,11 @@
- . ci/setup.sh
- cd build
- cmake -DBUILDNAME="$(uname -s)-AppleClang-8-Debug-${CI_BUILD_REF_NAME}" -DCMAKE_BUILD_TYPE=DEBUG -Dwasp_ENABLE_TESTS=ON -Dwasp_ENABLE_ALL_PACKAGES=ON ..
- ctest -D ExperimentalStart -D ExperimentalBuild -D ExperimentalTest -D ExperimentalSubmit
+. ci/setup.sh
+cd build
+cmake -DBUILDNAME="$(uname -s)-AppleClang-8-Debug-${CI_BUILD_REF_NAME}" \
+       -DCMAKE_BUILD_TYPE=DEBUG \
+       -Dwasp_ENABLE_TESTS=ON \
+       -Dwasp_ENABLE_ALL_PACKAGES=ON \
+       ..
+ctest -D ExperimentalStart \
+       -D ExperimentalBuild \
+       -D ExperimentalTest \
+       -D ExperimentalSubmit
