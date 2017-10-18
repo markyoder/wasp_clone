ef8de543eb0d4eb2108bccb2cde87420d8dd0ee1
Lefebvre, Robert A <lefebvrera@ornl.gov>
2017-10-18 14:13:42 -0400

    #39 fixing cmake invocation for intel 15. Updated intel 15 tag to require nsis

--- ci/windows_intel15_test.bat
+++ ci/windows_intel15_test.bat
@@ -2,7 +2,7 @@ call .\ci\setup.bat
 cd build
 set PATH=c:\vendors\cmake-ninja\bin;C:\Program Files (x86)\NSIS;%PATH%
 call "C:\Program Files (x86)\Intel\Composer XE 2015\bin\ipsxe-comp-vars.bat" intel64 vs2013
-cmake -DCMAKE_C_COMPILER=icl -D CMAKE_CXX_COMPILER=icl -DBUILDNAME="Windows-Intel-15.0-Release-%CI_BUILD_REF_NAME%" -DCMAKE_BUILD_TYPE:STRING=RELEASE -Dwasp_ENABLE_TESTS:BOOL=ON -Dwasp_ENABLE_ALL_PACKAGES:BOOL=ON -D wasp_ENABLE_INSTALL_CMAKE_CONFIG_FILES:BOOL=ON wasp_GENERATE_EXPORT_FILE_DEPENDENCIES:BOOL=ON -G "Ninja" ..
+cmake -DCMAKE_C_COMPILER=icl -D CMAKE_CXX_COMPILER=icl -DBUILDNAME="Windows-Intel-15.0-Release-%CI_BUILD_REF_NAME%" -DCMAKE_BUILD_TYPE:STRING=RELEASE -Dwasp_ENABLE_TESTS:BOOL=ON -Dwasp_ENABLE_ALL_PACKAGES:BOOL=ON -Dwasp_ENABLE_INSTALL_CMAKE_CONFIG_FILES:BOOL=ON -Dwasp_GENERATE_EXPORT_FILE_DEPENDENCIES:BOOL=ON -G "Ninja" ..
 ctest -D ExperimentalStart -D ExperimentalBuild -D ExperimentalTest -D ExperimentalSubmit
 copy /Y waspConfig_install.cmake install\lib\cmake\wasp\waspConfig.cmake
 ninja bundle
