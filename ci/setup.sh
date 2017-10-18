343e6a1d4558c1930f42dd3218c2d804eca4b78b
Lefebvre, Robert A <lefebvrera@ornl.gov>
2017-10-18 11:45:31 -0400

    #39 moving clang setup into ci/setup.sh

---
+++ ci/setup.sh
@@ -0,0 +1,10 @@
+ which git
+ git --version
+ git clone https://github.com/lefebvre/TriBITS.git TriBITS
+ module load cmake
+ which cmake
+ which python
+ python --version
+ python TriBITS/tribits/ci_support/clone_extra_repos.py
+ mkdir build
+ cd build
