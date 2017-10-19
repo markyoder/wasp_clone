7243bdf52d4eb2eb300c6997d0aca19326b62d6e
Lefebvre, Robert A <lefebvrera@ornl.gov>
2017-10-18 23:37:02 -0400

    #39 updating linux gcc test with bundle step and artifact capture.

--- ci/linux_gcc_test.sh
+++ ci/linux_gcc_test.sh
@@ -1,5 +1,37 @@
- . ci/setup.sh
- cd build
- module load valgrind
- cmake -DBUILDNAME="$(uname -s)-GCC-4.8.5-Release-${CI_BUILD_REF_NAME}" -DCMAKE_BUILD_TYPE=RELEASE -Dwasp_ENABLE_TESTS=ON -Dwasp_ENABLE_ALL_PACKAGES=ON ..
- ctest -D ExperimentalStart -D ExperimentalBuild -D ExperimentalTest -D ExperimentalSubmit
+. ci/setup.sh
+cd build
+module load valgrind
+cmake -DBUILDNAME="$(uname -s)-GCC-4.8.5-Release-${CI_BUILD_REF_NAME}" \
+      -DCMAKE_BUILD_TYPE=RELEASE \
+      -Dwasp_ENABLE_TESTS=ON \
+      -Dwasp_ENABLE_ALL_PACKAGES=ON \
+      ..
+
+ctest -D ExperimentalStart \
+      -D ExperimentalBuild -j 8\
+      -D ExperimentalSubmit \
+      -D ExperimentalTest -j 8 \
+      -D ExperimentalSubmit
+
+# clean up prior config for the next bundle config
+rm -rf CMake*
+cmake -DBUILDNAME="$(uname -s)-GCC-4.8.5-Bundle-${CI_BUILD_REF_NAME}" \
+      -DCPACK_PACKAGE_NAME=WASP \
+       -DCMAKE_BUILD_TYPE=RELEASE \
+       -Dwasp_ENABLE_ALL_PACKAGES=ON \
+	  -Dwasp_ENABLE_TESTS:BOOL=OFF \
+	  -Dwasp_ENABLE_ALL_PACKAGES:BOOL=ON \
+	  -Dwasp_ENABLE_testframework:BOOL=OFF \
+	  -Dwasp_ENABLE_googletest:BOOL=OFF \
+      -Dwasp_ENABLE_wasppy:BOOL=OFF \
+	  -Dwasp_ENABLE_INSTALL_CMAKE_CONFIG_FILES:BOOL=ON \
+	  -Dwasp_GENERATE_EXPORT_FILE_DEPENDENCIES:BOOL=ON \
+	  -Dwasp_ENABLE_CPACK_PACKAGING:BOOL=ON \
+      ..
+
+make -j 8 package
+ls -l ./
+# Copy bundle parts up to parent directory to avoid artifact
+# having build directory
+cp WASP-*-Linux.sh ../
+cp waspConfig_install.cmake ../
