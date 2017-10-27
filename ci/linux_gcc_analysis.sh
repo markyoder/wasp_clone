7106d2914332ada81dfc7b4978629aca9dd98f9f
Robert A Lefebvre <lefebvrera@ornl.gov>
2017-10-27 16:01:45 -0400

    Update linux_gcc_analysis.sh to separate arguments and reenable analysis.

--- ci/linux_gcc_analysis.sh
+++ ci/linux_gcc_analysis.sh
@@ -1,5 +1,5 @@
 . ci/setup.sh
 cd build
 module load valgrind
-cmake -MEMCHECK_COMMAND="$(which valgrind)" -DBUILDNAME="$(uname -s)-GCC-4.8.5-Debug-${CI_BUILD_REF_NAME}" -DCMAKE_BUILD_TYPE=DEBUG -Dwasp_ENABLE_COVERAGE_TESTING=ON -D COVERAGE_EXTRA_FLAGS="-s ${CI_PROJECT_DIR}/googletest -d"-Dwasp_ENABLE_TESTS=ON -Dwasp_ENABLE_ALL_PACKAGES=ON ..
+cmake -MEMCHECK_COMMAND="$(which valgrind)" -DBUILDNAME="$(uname -s)-GCC-4.8.5-Debug-${CI_BUILD_REF_NAME}" -DCMAKE_BUILD_TYPE=DEBUG -Dwasp_ENABLE_COVERAGE_TESTING=ON -D COVERAGE_EXTRA_FLAGS="-s ${CI_PROJECT_DIR}/googletest -d" -Dwasp_ENABLE_TESTS=ON -Dwasp_ENABLE_ALL_PACKAGES=ON ..
 ctest -D ExperimentalStart -D ExperimentalBuild -D ExperimentalTest -D ExperimentalMemCheck -D ExperimentalCoverage -D ExperimentalSubmit
