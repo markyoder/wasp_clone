70854fc2f7757b8feb17bc2f3f47998682f2afb9
Lefebvre, Robert A <lefebvrera@ornl.gov>
2018-01-23 11:19:02 -0500

    #97 updating ddi flexbison script to include sed for WASP_PUBLIC update.

--- waspddi/FlexBison.sh
+++ waspddi/FlexBison.sh
@@ -2,3 +2,4 @@ flex -Caf -v DDILexer.lex
 sed -i '' 's/#include <FlexLexer.h>/#include \"waspcore\/FlexLexer.h\"/' DDILexer.cpp 
 bison --version
 bison DDIParser.bison --report=state --report=lookahead --report=solved  -g
+sed -i '' 's/class DDIParser/class WASP_PUBLIC DDIParser/' DDIParser.hpp
