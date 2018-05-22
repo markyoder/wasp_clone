090d9d32c56124d53662a38fb802ff7a07498c74
Lefebvre, Robert A <lefebvrera@ornl.gov>
2018-05-22 15:15:35 -0400

    Updated wasp2py with reusable utility path function.

--- wasppy/wasp2py.py
+++ wasppy/wasp2py.py
@@ -1,26 +1,32 @@
 import subprocess, os, json
 from subprocess import Popen, PIPE
 
+def get_wasp_utility_path(utility_name):
+    path = __file__
+    if os.name == 'nt' :
+        ### for windows intel
+        path = os.path.dirname(__file__) + "\\..\\wasputils\\" + utility_name + ".exe"
+        if not os.path.isfile(path) :
+            ### for windows visual studio
+            path = os.path.dirname(__file__) + "\\..\\wasputils\\Release\\" + utility_name + ".exe"
+            if not os.path.isfile(path) :
+                ### for windows visual studio shared with CMAKE_RUNTIME_OUTPUT_DIRECTORY fixup
+                path = os.path.dirname(__file__) + "\\..\\cmake_runtime_output\\Release\\" + utility_name + ".exe"
+                if not os.path.isfile(path):
+                    path = os.path.dirname(__file__)+"\\..\\bin\\" + utility_name + ".exe"
+    else : 
+        path = os.path.dirname(__file__) + "/../wasputils/" + utility_name
+        if not os.path.isfile(path):
+            path = os.path.dirname(__file__)+"/../bin/" + utility_name
+    return path
+
 def get_json_dict(schema_filepath, input_filepath, ext=""):
     
     if   (ext == 'ddi') or (input_filepath[-4:] == '.ddi') : utility_name = 'ddivalidjson'
     else : utility_name = 'sonvalidjson' 
     
-    if os.name == 'nt' :
-        ### for windows intel
-        waspvalidjson = os.path.dirname(__file__) + "\\..\\wasputils\\" + utility_name + ".exe"
-        if not os.path.isfile(waspvalidjson) :
-            ### for windows visual studio
-            waspvalidjson = os.path.dirname(__file__) + "\\..\\wasputils\\Release\\" + utility_name + ".exe"
-            if not os.path.isfile(waspvalidjson) :
-                ### for windows visual studio shared with CMAKE_RUNTIME_OUTPUT_DIRECTORY fixup
-                waspvalidjson = os.path.dirname(__file__) + "\\..\\cmake_runtime_output\\Release\\" + utility_name + ".exe"
-                if not os.path.isfile(waspvalidjson):
-                    waspvalidjson = os.path.dirname(__file__)+"\\..\\bin\\" + utility_name + ".exe"
-    else : 
-        waspvalidjson = os.path.dirname(__file__) + "/../wasputils/" + utility_name
-        if not os.path.isfile(waspvalidjson):
-            waspvalidjson = os.path.dirname(__file__)+"/../bin/" + utility_name
+    waspvalidjson = get_wasp_utility_path(utility_name)
+    
     cmd = waspvalidjson + ' ' + schema_filepath + ' ' + input_filepath
     proc = Popen(cmd, shell=True, stdout=PIPE)
     json_result = proc.communicate()[0]
