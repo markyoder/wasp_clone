20bad3faf7030878d299924bc5d1371dc85bfae5
Lefebvre, Robert A <lefebvrera@ornl.gov>
2018-09-11 14:26:51 -0400

    Updating siren flex/bison logic to ensure GNU Bison parser is public.

--- waspsiren/FlexBison.sh
+++ waspsiren/FlexBison.sh
@@ -2,5 +2,6 @@ flex -Caf -v SIRENLexer.lex
 sed -i '' 's/#include <FlexLexer.h>/#include \"waspcore\/FlexLexer.h\"/' SIRENLexer.cpp 
 bison --version
 bison SIRENParser.bison --report=state --report=lookahead --report=solved  -g
+sed -i '' 's/class SIRENParser/class WASP_PUBLIC SIRENParser/' SIRENParser.hpp
 #mv SIRENParser.hpp SIRENParser.h
 #sed -i '' 's/#include "SIRENParser.hpp"/#include \"SIRENParser.h\"/' SIRENParser.cpp 
