c70c3c10d1509329b547afac6220fde477e5f3ee
Lefebvre, Robert A <lefebvrera@ornl.gov>
2019-04-08 18:06:56 -0400

    Adding initial GCC 5.4 testing script.

---
+++ ci/linux_gcc5.4_test.sh
@@ -0,0 +1,17 @@
+. ci/setup.sh
+cd build
+. /projects/gcc-5.4.0/load_dev_env.sh
+cmake -DBUILDNAME="$(uname -s)-GCC-5.4-Release-${CI_BUILD_REF_NAME}" \
+      -DCMAKE_BUILD_TYPE=RELEASE \
+      -Dwasp_ENABLE_TESTS=ON \
+      -DCMAKE_C_COMPILER=/projects/gcc-5.4.0/toolset/gcc-5.4.0/bin/gcc \
+      -DCMAKE_CXX_COMPILER=/projects/gcc-5.4.0/toolset/gcc-5.4.0/bin/g++ \
+      -DBUILD_SHARED_LIBS:BOOL=ON \
+      -Dwasp_ENABLE_ALL_PACKAGES=ON \
+      ..
+
+ctest -D ExperimentalStart \
+      -D ExperimentalBuild -j 8\
+      -D ExperimentalSubmit \
+      -D ExperimentalTest -j 8 \
+      -D ExperimentalSubmit
