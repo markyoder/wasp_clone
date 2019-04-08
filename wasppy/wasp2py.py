d572a2b432c82008c1951141cb43f76810f847e7
Robert A Lefebvre <lefebvrera@ornl.gov>
2019-04-08 18:03:56 -0400

    Update wasp2py.py to include the connection to the viivalidjson utility

--- wasppy/wasp2py.py
+++ wasppy/wasp2py.py
@@ -23,6 +23,7 @@ def get_wasp_utility_path(utility_name):
 def get_json_dict(schema_filepath, input_filepath, ext=""):
     
     if   (ext == 'ddi') or (input_filepath[-4:] == '.ddi') : utility_name = 'ddivalidjson'
+    elif (ext=="vii") : utility_name="viivalidjson"
     else : utility_name = 'sonvalidjson' 
     
     waspvalidjson = get_wasp_utility_path(utility_name)
