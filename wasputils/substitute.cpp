f1dc9f72185c86252ba6df9a6dc207e8c9f79c6a
Langley, Brandon R <langleybr@ornl.gov>
2019-07-19 19:56:54 -0400

    made the substitute utility determine the files base on order instead of file extension and made default delimiters '{' and '}' - to act like dakota's dprepro

--- wasputils/substitute.cpp
+++ wasputils/substitute.cpp
@@ -27,65 +27,67 @@ int main(int argc, char** argv)
 
     if (argc < 2)
     {
-        std::cout << "Usage: " << std::endl;
-        std::cout << "\t" << argv[0] << " template [dakota_params.in] " << std::endl;
-        std::cout << "\ti.e., " << argv[0]
-                  << " /path/to/definition.tmpl /path/to/some/dakota_params.in[.#] /path/to/the/results.out[.#] --ldelim \"<\" --rdelim \">\" --hop \".\""
-                  << std::endl;
-        std::cout << " Usage : " << argv[0]
-                  << " --version\t(print version info)" << std::endl
-                  << " --ldelim\tset the attribute's left delimiter" << std::endl
-                  << " --rdelim\tset the attribute's right delimiter" << std::endl
-                  << " --hop\tset the attribute's hierarchical delimiter"<< std::endl;
+        std::cout << "Usage: " << std::endl << "    " << argv[0]
+                  << R"I( [options] parameters_file template_input_file [new_input_file]
+
+Options:
+
+    --left-delimiter, --right-delimiter
+        override the default curly brace delimiters { and }; these may need
+        to be quoted or escaped on the command line
+
+e.g., )I" << argv[0] << R"I( dakota_params.in definition.tmpl results.out
+      ( put the new input file in results.out - using default { and } delimiters )
+
+e.g., )I" << argv[0] << R"I( --left-delimiter "<" --right-delimiter ">" dakota_params.in definition.tmpl
+      ( print the new input file to the screen - using overridden < and > delimiters )
+
+e.g., )I" << argv[0] << R"I( --version
+      ( print version info )
+)I" << std::endl;
         return 1;
     }
 
-    std::string tmpl = argv[1];
-    std::string params = "";
+    std::string params  = "";
+    std::string tmpl    = "";
     std::string results = "";
-    std::string ldelim = "<";
-    std::string rdelim = ">";
+
+    bool params_found  = false;
+    bool tmpl_found    = false;
+    bool results_found = false;
+
+    std::string ldelim = "{";
+    std::string rdelim = "}";
     std::string hop    = ".";
 
     for ( int i = 1; i < argc; ++i )
     {
         std::string arg = argv[i];
-        if (arg.size() > 3 && arg.substr(arg.size() - 3) == ".in")
-        {
-            params = arg;
-        }
-        else if (arg.size() > 4 && arg.substr(arg.size() - 4) == ".out")
-        {
-            results = arg;
-        }
-        else if (arg.size() > 5 && arg.substr(arg.size() - 5) == ".tmpl")
-        {
-            tmpl = arg;
-        }
-        else if (arg == "--ldelim" && i+1 < argc)
+
+        if ( arg == "--left-delimiter" && i+1 < argc )
         {
             ldelim = argv[i+1];
             i++;
         }
-        else if (arg == "--rdelim" && i+1 < argc)
+        else if ( arg == "--right-delimiter" && i+1 < argc )
         {
             rdelim = argv[i+1];
             i++;
         }
-        else if (arg == "--hop" && i+1 < argc)
-        {
-            hop = argv[i+1];
-            i++;
-        }
-        // Parameter files can be '*.in.#'
-        else if( arg.find(".in.") != std::string::npos)
+        else if ( !params_found )
         {
             params = arg;
+            params_found = true;
         }
-        // Result files can be '*.out.#'
-        else if( arg.find(".out.") != std::string::npos)
+        else if ( !tmpl_found )
+        {
+            tmpl = arg;
+            tmpl_found = true;
+        }
+        else if ( !results_found )
         {
             results = arg;
+            results_found = true;
         }
         else
         {
@@ -93,6 +95,28 @@ int main(int argc, char** argv)
                     <<arg<<"'"<<std::endl;
             return 1;
         }
+    }
+
+    if ( !params_found )
+    {
+        std::cerr << std::endl
+                  << "***Error: No parameters_file provided on command line"
+                  << std::endl
+                  << " - Usage: " << argv[0]
+                  << " [options] parameters_file template_input_file new_input_file"
+                  << std::endl << std::endl;
+        return 1;
+    }
+
+    if ( !tmpl_found )
+    {
+        std::cerr << std::endl
+                  << "***Error: No template_input_file provided on command line"
+                  << std::endl
+                  << " - Usage: " << argv[0]
+                  << " [options] parameters_file template_input_file new_input_file"
+                  << std::endl << std::endl;
+        return 1;
     }
 
     bool success = false;
