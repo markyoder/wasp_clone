9c69511bb1c327a7c08f51d62cf7fc347b09027d
Baird, Mark L <bairdml@ornl.gov>
2020-08-11 19:18:19 -0400

    Adding logic to select python version

--- ci/linux_gcc5.4_test.sh
+++ ci/linux_gcc5.4_test.sh
@@ -3,9 +3,11 @@ echo "${PYTHON_VERSION}"
 if [[ ${PYTHON_VERSION} = "python3" ]]; then
 export PATH=/projects/gcc-5.4.0/common_tools/anaconda3/bin/:$PATH
   ./projects/gcc-5.4.0/load_dev_env_python3.sh
+  echo "VER ${PYTHON_VERSION}"
 else
 export PATH=/projects/gcc-5.4.0/common_tools/anaconda2/bin/:$PATH
  . /projects/gcc-5.4.0/load_dev_env.sh
+ echo "VER ${PYTHON_VERSION}"
 fi
 
 . ci/setup.sh
