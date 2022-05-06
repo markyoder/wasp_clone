a40a14239e70d39b583391d227e7953c9c931b1c
Baird M L <inb@warroom9.ornl.gov>
2022-05-06 14:30:33 -0400

    Adding PYTHON3 install for wasp testing for VS shared build

--- ci/vs2013_shared_test.bat
+++ ci/vs2013_shared_test.bat
@@ -1,7 +1,13 @@
 call .\ci\setup.bat
 cd build
-set "PATH=C:/Python27;%PATH%"
+set "conda=%cd%"
+git clone https://code.ornl.gov/casl/anaconda.git
+%conda%\anaconda\Anaconda3-2021.11-Windows-x86_64.exe /S /D=%conda%
+set "PATH=%conda%\Scripts;%conda%;%conda%\Library\bin;%PATH%"
+
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+
+
 cmake -DBUILD_SHARED_LIBS=ON ^
       -DBUILDNAME="Windows-CL-18-Shared-Release-%CI_BUILD_REF_NAME%" ^
       -DCMAKE_BUILD_TYPE:STRING=RELEASE ^
