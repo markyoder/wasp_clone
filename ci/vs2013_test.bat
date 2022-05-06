ce0a3aa13614fafdcd62ce85432da64f28d26171
Baird M L <inb@warroom9.ornl.gov>
2022-05-06 15:04:49 -0400

    Adding PYTHON3 to VS2013 build

--- ci/vs2013_test.bat
+++ ci/vs2013_test.bat
@@ -1,6 +1,9 @@
 call .\ci\setup.bat
 cd build
-set "PATH=C:/Python27;%PATH%"
+set "conda=%cd%"
+git clone https://code.ornl.gov/casl/anaconda.git
+%conda%\anaconda\Anaconda3-2021.11-Windows-x86_64.exe /S /D=%conda%
+set "PATH=%conda%\Scripts;%conda%;%conda%\Library\bin;%PATH%"
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 cmake -DBUILDNAME="Windows-CL-18-Release-%CI_BUILD_REF_NAME%" ^
       -DCMAKE_BUILD_TYPE:STRING=RELEASE ^
