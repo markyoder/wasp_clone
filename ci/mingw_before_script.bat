137dd06be3add8343d5894e807f329bada6bc203
Baird M L <inb@warroom9.ornl.gov>
2022-05-07 00:01:40 -0400

    Updating minigw pre script

--- ci/mingw_before_script.bat
+++ ci/mingw_before_script.bat
@@ -1,1 +1,2 @@
-set PATH=%PATH%;c:\vendors\mingw-w64\x86_64-4.8.5-posix-seh-rt_v4-rev0\mingw64\bin;c:\vendors\git\2.11\cmd;c:\vendors\cmake\3.6.3\bin;c:\Python27;
+set PATH=%PATH%;c:\vendors\mingw-w64\x86_64-4.8.5-posix-seh-rt_v4-rev0\mingw64\bin;c:\vendors\miniconda3\conda\bin;c:\vendors\git\2.11\cmd;c:\vendors\cmake\3.6.3\bin;c:\Python27;
+dir c:\vendors\mingw-w64\x86_64-4.8.5-posix-seh-rt_v4-rev0\mingw64\bin
