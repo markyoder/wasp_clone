6dc9d6ac503eb554208da2be15e20a3832f825e7
Baird M L <inb@warroom9.ornl.gov>
2022-08-17 12:41:58 -0400

    Updating the Windows and Mac miniconda repo location.

--- ci/vs2013_test.bat
+++ ci/vs2013_test.bat
@@ -2,7 +2,7 @@ call .\ci\setup.bat
 cd build
 set "base=%cd%"
 set conda=%base%\conda
-git clone git@code-int.ornl.gov:warroom/miniconda.git
+git clone git@code.ornl.gov:warroom/miniconda.git
 %base%\miniconda\Miniconda3-latest-Windows-x86_64.exe /S /D=%conda%
 set "PATH=%conda%\Scripts;%conda%;%conda%\Library\bin;%PATH%"
 where python
