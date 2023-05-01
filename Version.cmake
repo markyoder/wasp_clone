86583f3b36986f1778940b940f2e31e5f65ffe70
Lefebvre, Robert A <lefebvrera@ornl.gov>
2023-05-01 15:55:54 -0400

    Update Version and changelog for 3.1.0

--- Version.cmake
+++ Version.cmake
@@ -6,11 +6,11 @@
 # for release mode and set the version.
 
 SET(${PROJECT_NAME}_NAME "wasp")
-SET(${PROJECT_NAME}_VERSION 3.0.2)
-SET(${PROJECT_NAME}_FULL_VERSION "Workbench Analysis Sequence Processor 3.0.2")
-SET(${PROJECT_NAME}_VERSION_STRING "3.0.2")
+SET(${PROJECT_NAME}_VERSION 3.0.3)
+SET(${PROJECT_NAME}_FULL_VERSION "Workbench Analysis Sequence Processor 3.0.3")
+SET(${PROJECT_NAME}_VERSION_STRING "3.0.3")
 SET(${PROJECT_NAME}_MAJOR_VERSION 03)
-SET(${PROJECT_NAME}_MAJOR_MINOR_VERSION 030002)
+SET(${PROJECT_NAME}_MAJOR_MINOR_VERSION 030003)
 SET(${PROJECT_NAME}_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release (beta or final)
 
 ##---------------------------------------------------------------------------##
