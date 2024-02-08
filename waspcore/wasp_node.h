a58c5354510b1c31b7f93115260310f74c77b3b3
Robert A. Lefebvre <lefebvrera@ornl.gov>
2024-02-08 10:26:21 -0500

    Fixup formatting of WASP node types

--- waspcore/wasp_node.h
+++ waspcore/wasp_node.h
@@ -24,67 +24,40 @@ enum NODE : unsigned char
     OBJECT_TERM,
     OBJECT,
     SEMICOLON,  // ;
-    WASP_COMMA  // ,
-    ,
+    WASP_COMMA,  // ,
     COLON,
-    PLUS  // +
-    ,
-    MINUS  // -
-    ,
-    MULTIPLY  // *
-    ,
-    DIVIDE  // /
-    ,
-    GTE  // >=
-    ,
-    GT  // >
-    ,
-    LTE  // <=
-    ,
-    LT  // >
-    ,
-    NEQ  // !=
-    ,
-    EQ  // ==
-    ,
-    WASP_AND  // && // disambiguated for cross-platform macro avoidance
-    ,
-    WASP_OR  // || // disambiguated for cross-platform macro avoidance
-    ,
-    BANG  // !
-    ,
-    UNARY_MINUS  // -x
-    ,
-    UNARY_NOT  // !x
-    ,
-    EXPONENT  // ^
-    ,
-    LPAREN  // (
-    ,
-    RPAREN  // )
-    ,
-    LBRACE  // {
-    ,
-    RBRACE  // }
-    ,
+    PLUS,  // +
+    MINUS,  // -
+    MULTIPLY,  // *
+    DIVIDE,  // /
+    GTE,  // >=
+    GT,  // >
+    LTE,  // <=
+    LT,  // >
+    NEQ,  // !=
+    EQ,  // ==
+    WASP_AND,  // && // disambiguated for cross-platform macro avoidance
+    WASP_OR,  // || // disambiguated for cross-platform macro avoidance
+    BANG,  // !
+    UNARY_MINUS,  // -x
+    UNARY_NOT,  // !x
+    EXPONENT,  // ^
+    LPAREN,  // (
+    RPAREN,  // )
+    LBRACE,  // {
+    RBRACE,  // }
     PARENTHESIS,
     FUNCTION,
     EXPRESSION,
-    LBRACKET  // [
-    ,
-    RBRACKET  // ]
-    ,
+    LBRACKET,  // [
+    RBRACKET,  // ]
     DOT_SLASH,
-    WASP_TRUE  // disambiguated for cross-platform macro avoidance
-    ,
-    WASP_FALSE  // disambiguated for cross-platform macro avoidance
-    ,
+    WASP_TRUE,  // disambiguated for cross-platform macro avoidance
+    WASP_FALSE,  // disambiguated for cross-platform macro avoidance
     FILL_EXPR,
-    WASP_TICK  // `
-    ,
+    WASP_TICK,  // `
     IDENTIFIER,
-    ARRAY  // sequence of data
-    ,
+    ARRAY,  // sequence of data
     EXECUTION_UNIT_END,
     EXECUTION_UNIT_START,
     EXECUTION_UNIT,
@@ -94,8 +67,7 @@ enum NODE : unsigned char
     PREDICATED_CHILD,
     INDEX,
     ANY,
-    WASP_NULL  // disambiguated from C-NULL
-    ,
+    WASP_NULL,  // disambiguated from C-NULL
     CONDITIONAL,
     FILE,
     REPEAT
