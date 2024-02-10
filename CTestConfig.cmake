eb142f21e5d6517f9f131ef34dd48b93075079d1
Lefebvre, Robert Alexander <lefebvrera@ornl.gov>
2024-02-10 13:11:30 +0000

    Update CDash URL

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,9 +8,9 @@
 SET(CTEST_NIGHTLY_START_TIME "02:00:00 UTC")
 SET(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 200)
 set(CTEST_PROJECT_NAME "wasp")
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "ci.ornl.gov")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=wasp")
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "cdash.ornl.gov")
+set(CTEST_DROP_LOCATION "/submit.php?project=WASP")
 set(CTEST_TRIGGER_SITE "")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
