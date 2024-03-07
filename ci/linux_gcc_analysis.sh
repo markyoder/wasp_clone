0f1fefed5b0eed07b824928eff758dcec8c71e8a
Robert A. Lefebvre <lefebvrera@ornl.gov>
2024-03-06 21:16:30 -0500

    Update analysis script to no longer rely system configuration

--- ci/linux_gcc_analysis.sh
+++ ci/linux_gcc_analysis.sh
@@ -1,10 +1,18 @@
 . ci/setup.sh
 cd build
-module load valgrind
+
+# Setup a conda install
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${PWD}/miniconda3/miniconda.sh
+bash ${PWD}/miniconda3/miniconda.sh -b -u -p ${PWD}/miniconda3
+eval "$(${PWD}/miniconda3/bin/conda shell.bash hook 2> /dev/null)"
+conda env create -f ../ci/env.yml
+conda activate wasp_ci
+conda install -c conda-forge valgrind
+
 cmake -MEMCHECK_COMMAND="$(which valgrind)" \
-      -DBUILDNAME="$(uname -s)-GCC-4.8.5-Debug-${CI_COMMIT_REF_NAME}" \
+      -DBUILDNAME="$(uname -s)-Analysis-Debug-${CI_COMMIT_REF_NAME}" \
       -DCMAKE_BUILD_TYPE=DEBUG \
-      -Dwasp_ENABLE_SWIG=ON \
+      -DWASP_ENABLE_SWIG=ON \
       -Dwasp_ENABLE_COVERAGE_TESTING=ON \
       -DCOVERAGE_EXTRA_FLAGS="-s ${CI_PROJECT_DIR}/googletest -d" \
       -Dwasp_ENABLE_TESTS=ON \
